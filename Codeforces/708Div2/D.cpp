7cacbe499e8d5018f1482ac352ba700c3c28899e
zerotrac <zerotrac2259@gmail.com>
2021-03-24 13:48:23 +0800

    ARC115 E & CF708 Div2

--- Codeforces/708Div2/D.cpp
+++ Codeforces/708Div2/D.cpp
@@ -34,8 +34,30 @@ inline void quickread() {
     cin.tie(nullptr);
 }
 
-inline void work() {
+int n;
+int a[5010], b[5010];
+LL f[5010];
 
+inline void work() {
+    cin >> n;
+    for (int i = 1; i <= n; ++i) {
+        cin >> a[i];
+    }
+    for (int i = 1; i <= n; ++i) {
+        cin >> b[i];
+        f[i] = 0;
+    }
+    for (int i = 2; i <= n; ++i) {
+        for (int j = i - 1; j >= 1; --j) {
+            if (a[i] != a[j]) {
+                LL fi = f[j] + abs(b[i] - b[j]);
+                LL fj = f[i] + abs(b[i] - b[j]);
+                f[i] = max(f[i], fi);
+                f[j] = max(f[j], fj);
+            }
+        }
+    }
+    cout << *max_element(f + 1, f + n + 1) << "\n";
 }
 
 int main() {
